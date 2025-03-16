6c092fce171c980326b4e38cc7f1b25b15ae507a
nare228 <gevnar21@mail.ru>
2025-03-16 18:01:55 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,14 +24,15 @@ if data is not None and api:
             temperature = request['main']['temp']
             wind_speed = request['wind']['speed']
             description = request['weather'][0]['description']
+            st.write(f"Температура в {city}: {temperature}°C, Описание: {description}, Скорость ветра: {wind_speed} м/с")
             season = data[data['city'] == city]
-            average = data[data['season'] == season['season'].mode()[0]]['temperature'].mean()
-            seasonal_std = data[data['season'] == season['season'].mode()[0]]['temperature'].std()
+            average = season['temperature'].mean()
+            seasonal_std = season['temperature'].std()
             temperature_limits = (average - 2 * seasonal_std, average + 2 * seasonal_std)
             season['anomaly'] = ~season['temperature'].between(temperature_limits[0], temperature_limits[1])
             plt.figure(figsize=(12, 6))
-            plt.plot(season['timestamp'], season['temperature'], marker='o', label='Температура', color='blue')
-            plt.scatter(season[season['anomaly']]['timestamp'],season[season['anomaly']]['temperature'], color='red', marker='o', label='Аномальные значения')
+            sns.lineplot(data=season, x='timestamp', y='temperature', marker='o', label='Температура', color='blue')
+            plt.scatter(season[season['anomaly']]['timestamp'], season[season['anomaly']]['temperature'], color='red', label='Аномальные значения', s=100, edgecolor='black')
             plt.axhline(y=temperature_limits[0], color='green', linestyle='--', label='Нижний предел')
             plt.axhline(y=temperature_limits[1], color='orange', linestyle='--', label='Верхний предел')
             plt.title(f'Временной ряд температуры в {city} с аномалиями')
@@ -39,14 +40,15 @@ if data is not None and api:
             plt.ylabel("Температура (°C)")
             plt.xticks(rotation=45)
             plt.legend()
-            plt.tight_layout()  # Обеспечивает лучшее размещение графика
+            plt.tight_layout()
             st.pyplot(plt)
+            season_avgs = data.groupby('season')['temperature'].mean().reset_index()
             plt.figure(figsize=(12, 6))
-            sns.lineplot(data=data, x='season', y='temperature', estimator='mean', marker='o', palette='Set2')
+            sns.barplot(data=season_avgs, x='season', y='temperature', palette='Set2', ci=None)
             plt.title(f'Cредняя температура по сезонам для {city}')
             plt.xlabel('Сезон')
             plt.ylabel('Средняя температура (°C)')
-            plt.grid()
+            plt.xticks(rotation=45)
             st.pyplot(plt)
         else:
             st.error('{"cod":401, "message": "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."}')
