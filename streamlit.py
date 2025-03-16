2bdd64d4aa5fac17d0d9d92da93035d9d3ee68e9
nare228 <gevnar21@mail.ru>
2025-03-16 17:52:53 +0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 cities = ['London', 'Paris', 'New York', 'Tokyo', 'Sydney', 'Moscow', 'Beijing','Cairo', 'Rio de Janeiro', 'Mumbai','Mexico City', 'Dubai', 'Los Angeles', 'Singapore']
-st.title("Погодное приложение от Нарэ")
+st.title("Streamlit Нарэ")
 city = st.selectbox("Выберите город", cities)
 with st.form('api'):
     api = st.text_input('Введите API-ключ')
@@ -28,25 +28,25 @@ if data is not None and api:
             average = data[data['season'] == season['season'].mode()[0]]['temperature'].mean()
             seasonal_std = data[data['season'] == season['season'].mode()[0]]['temperature'].std()
             temperature_limits = (average - 2 * seasonal_std, average + 2 * seasonal_std)
-            if temperature_limits[0] <= temperature <= temperature_limits[1]:
-                st.success(f"Температура в {city}: {temperature}°C — Не аномальная, "f"{description}, Скорость  ветра{wind_speed} м/с")
-            else:
-                st.warning(f"Температура в {city}: {temperature}°C — Аномальная, "f"{description}, Скорость ветра: {wind_speed} м/с")
+            season['anomaly'] = ~season['temperature'].between(temperature_limits[0], temperature_limits[1])
             plt.figure(figsize=(12, 6))
             plt.plot(season['timestamp'], season['temperature'], marker='o', label='Температура', color='blue')
-            mean = data[data['season'] == season['season'].mode()[0]]['temperature'].mean()
-            std = data[data['season'] == season['season'].mode()[0]]['temperature'].std()
-            plt.fill_between(season['timestamp'],mean - 2 * std,mean + 2 * std, color='green', alpha=0.3, label='Доверительный интервал')
-            plt.title(f'Временной ряд температуры в {city}')
+            plt.scatter(season[season['anomaly']]['timestamp'],season[season['anomaly']]['temperature'], color='red', marker='o', label='Аномальные значения')
+            plt.axhline(y=temperature_limits[0], color='green', linestyle='--', label='Нижний предел')
+            plt.axhline(y=temperature_limits[1], color='orange', linestyle='--', label='Верхний предел')
+            plt.title(f'Временной ряд температуры в {city} с аномалиями')
             plt.xlabel("Дата")
             plt.ylabel("Температура (°C)")
             plt.xticks(rotation=45)
             plt.legend()
+            plt.tight_layout()  # Обеспечивает лучшее размещение графика
             st.pyplot(plt)
-            season_avgs = data.groupby('season')['temperature'].mean().reset_index()
-            plt.figure(figsize=(8, 8))
-            plt.pie(season_avgs['temperature'], labels=season_avgs['season'], autopct='%1.1f%%', colors=sns.color_palette('Set2'))
-            plt.title(f'Средняя температура по сезонам для {city}')
+            plt.figure(figsize=(12, 6))
+            sns.lineplot(data=data, x='season', y='temperature', estimator='mean', marker='o', palette='Set2')
+            plt.title(f'Cредняя температура по сезонам для {city}')
+            plt.xlabel('Сезон')
+            plt.ylabel('Средняя температура (°C)')
+            plt.grid()
             st.pyplot(plt)
         else:
             st.error('{"cod":401, "message": "Invalid API key. Please see https://openweathermap.org/faq#error401 for more info."}')
